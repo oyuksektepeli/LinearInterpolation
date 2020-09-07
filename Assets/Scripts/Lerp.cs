a591acdf13aaa7a54588fb29daf75e9bf8e7bc4f
Onur <onuryuksektepeli@hotmail.com.tr>
2020-09-07 20:14:45 +0300

    Initial commit

---
+++ Assets/Scripts/Lerp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerp : MonoBehaviour
+{
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
